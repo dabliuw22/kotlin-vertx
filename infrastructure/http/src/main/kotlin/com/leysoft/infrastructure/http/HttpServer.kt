e481da578bb31ffab16e9f2b22ec7ebe99c50b90
Will Leyton <wleylop22@gmail.com>
2020-10-12 13:03:12 -0500

    Add cors

--- infrastructure/http/src/main/kotlin/com/leysoft/infrastructure/http/HttpServer.kt
+++ infrastructure/http/src/main/kotlin/com/leysoft/infrastructure/http/HttpServer.kt
@@ -3,16 +3,26 @@ package com.leysoft.infrastructure.http
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
 import io.vertx.core.Promise
+import io.vertx.core.http.HttpMethod
 import io.vertx.core.http.HttpServerOptions
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.ext.web.handler.CorsHandler
 
 abstract class HttpServer : AbstractVerticle() {
+
+    abstract fun port(): Int
+
+    abstract fun host(): String
+
+    abstract fun routes(router: Router)
 
     override fun start(startFuture: Future<Void>?) {
         val promise: Promise<Void> = Promise.promise()
         val router: Router = Router.router(vertx)
-        router.route().handler(BodyHandler.create())
+        router.route()
+            .handler(BodyHandler.create())
+            .handler(cors())
         routes(router)
         vertx.createHttpServer(options())
             .requestHandler(router)
@@ -22,14 +32,22 @@ abstract class HttpServer : AbstractVerticle() {
             }
     }
 
+    private fun cors(): CorsHandler =
+        CorsHandler.create("*")
+            .allowedMethods(
+                setOf(
+                    HttpMethod.DELETE,
+                    HttpMethod.GET,
+                    HttpMethod.OPTIONS,
+                    HttpMethod.PATCH,
+                    HttpMethod.POST,
+                    HttpMethod.PUT
+                )
+            )
+            .allowedHeader("Content-Type")
+
     private fun options(): HttpServerOptions =
         HttpServerOptions()
             .setCompressionSupported(true)
             .setLogActivity(true)
-
-    abstract fun port(): Int
-
-    abstract fun host(): String
-
-    abstract fun routes(router: Router)
 }
