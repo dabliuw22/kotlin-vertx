ec31adb5d32b149c18389af34886bed4ef416171
Will Leyton <wleylop22@gmail.com>
2022-08-04 19:56:29 -0500

    Update

--- infrastructure/logger/src/main/kotlin/com/leysoft/infrastructure/logger/LoggerFactory.kt
+++ infrastructure/logger/src/main/kotlin/com/leysoft/infrastructure/logger/LoggerFactory.kt
@@ -3,41 +3,28 @@ package com.leysoft.infrastructure.logger
 import arrow.Kind
 import arrow.fx.typeclasses.Effect
 import org.slf4j.LoggerFactory
-import kotlin.reflect.KClass
 
 object LoggerFactory {
-
     inline fun <reified A, F> getLogger(Q: Effect<F>): Logger<F> =
-        DefaultLogger.make(Q, A::class)
+        object : Logger<F> {
+            private val logger = LoggerFactory.getLogger(A::class.java)
 
-    class DefaultLogger<F> private constructor(
-        private val Q: Effect<F>,
-        clazz: KClass<*>
-    ) : Logger<F> {
+            override fun info(message: String): Kind<F, Unit> =
+                Q.defer { Q.effect { logger.info(message) } }
 
-        private val logger = LoggerFactory.getLogger(clazz.java)
+            override fun error(message: String): Kind<F, Unit> =
+                Q.defer { Q.effect { logger.error(message) } }
 
-        override fun info(message: String): Kind<F, Unit> =
-            Q.defer { Q.effect { logger.info(message) } }
+            override fun error(message: String, throwable: Throwable): Kind<F, Unit> =
+                Q.defer { Q.effect { logger.error(message, throwable) } }
 
-        override fun error(message: String): Kind<F, Unit> =
-            Q.defer { Q.effect { logger.error(message) } }
+            override fun debug(message: String): Kind<F, Unit> =
+                Q.defer { Q.effect { logger.debug(message) } }
 
-        override fun error(message: String, throwable: Throwable): Kind<F, Unit> =
-            Q.defer { Q.effect { logger.error(message, throwable) } }
+            override fun warn(message: String): Kind<F, Unit> =
+                Q.defer { Q.effect { logger.warn(message) } }
 
-        override fun debug(message: String): Kind<F, Unit> =
-            Q.defer { Q.effect { logger.debug(message) } }
-
-        override fun warn(message: String): Kind<F, Unit> =
-            Q.defer { Q.effect { logger.warn(message) } }
-
-        override fun trace(message: String): Kind<F, Unit> =
-            Q.defer { Q.effect { logger.trace(message) } }
-
-        companion object {
-            fun <F> make(Q: Effect<F>, clazz: KClass<*>): Logger<F> =
-                DefaultLogger(Q, clazz)
+            override fun trace(message: String): Kind<F, Unit> =
+                Q.defer { Q.effect { logger.trace(message) } }
         }
-    }
 }
