bb5af49f82c8c2683b4ff104bdcd151a64c53dc3
Will Leyton <wleylop22@gmail.com>
2024-05-08 11:02:07 -0500

    Update error handler

--- infrastructure/http/src/main/kotlin/com/leysoft/infrastructure/http/error.kt
+++ infrastructure/http/src/main/kotlin/com/leysoft/infrastructure/http/error.kt
@@ -1,10 +1,27 @@
 package com.leysoft.infrastructure.http
 
-import io.ktor.server.application.ApplicationCall
+import com.leysoft.core.error.*
+import io.ktor.http.*
 
-typealias HttpErrorHandler<A> =
-    ApplicationCall.() -> suspend (A) -> Unit
+typealias HttpErrorHandler<A, B> = (A) -> B
 
 data class ErrorResponse(
-    val message: String
+    val message: String,
+    val code: HttpStatusCode
 )
+
+val throwableHandler: HttpErrorHandler<Throwable, ErrorResponse> = {
+        throwable ->
+    ErrorResponse(throwable.message ?: "Internal Server Error", HttpStatusCode.InternalServerError)
+}
+
+val errorHandler: HttpErrorHandler<BaseException, ErrorResponse> =
+    { error ->
+        when (error) {
+            is NotFoundProductException -> ErrorResponse(error.message, HttpStatusCode.NotFound)
+            is CreateProductException -> ErrorResponse(error.message, HttpStatusCode.Conflict)
+            is DeleteProductException -> ErrorResponse(error.message, HttpStatusCode.Conflict)
+            is CustomProductException -> ErrorResponse(error.message, HttpStatusCode.Conflict)
+            else -> ErrorResponse("Internal Server Error", HttpStatusCode.InternalServerError)
+        }
+    }
